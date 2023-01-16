e514038277af66dd7d6d8c59564bdf83691630dc
Danil Tkachenko <tckachencko.danil2013@yandex.ru>
2023-01-16 13:44:02 +0300

    initial commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Леонид Шелудько on 16.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
